9cfd201d92a14fd4bcf5ae99d8daf02d7c3bc26d
Nathan Martinez <n.mtz.cas@gmail.com>
2023-06-27 22:32:01 -0600

    Application skeleton is set up

---
+++ services/tasks_api/tests.py
@@ -0,0 +1,16 @@
+import pytest 
+from fastapi import status
+from starlette.testclient import TestClient
+
+from main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_health_check(client):
+    response = client.get("/api/health-check/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message":"OK"}
